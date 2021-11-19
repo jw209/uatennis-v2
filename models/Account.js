930daf7a48aea09995d3371ac42882586ea965bc
jw209 <jw209@uakron.edu>
2021-11-19 14:19:58 -0500

    Create and initialize Account schema

---
+++ models/Account.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mognoose.Schema;
+
+const AccountSchema = new Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    uanetid: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Account = mongoose.model("accounts", AccountSchema);
