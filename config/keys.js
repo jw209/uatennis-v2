1182d43d4ec63fa8226c92ccd4ca391c9e161b4e
jw209 <jw209@uakron.edu>
2021-11-19 14:56:53 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://jw209:ThC419plusOne%3F@cluster0.ghlu9.mongodb.net/Tennis?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://jw209:ThC419plusOne%3F@cluster0.ghlu9.mongodb.net/Tennis?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
