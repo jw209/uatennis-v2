412ca34a678e0b391728ee993d1ac2be978d8c05
jw209 <jw209@uakron.edu>
2021-11-28 04:30:43 -0500

    Login redirects to home rather than chat

---
+++ models/Game.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// schema
+const Schema = mongoose.Schema;
+const GameSchema = new Schema({
+    oppfname: String,
+    opplname: String,
+    set1you: String,
+    set1opp: String,
+    set2you: String,
+    set2opp: String,
+    set3you: String,
+    set3opp: String,
+    date: String
+});
+
+// model
+const Game = mongoose.model('Game', GameSchema);
+
+module.exports = Game;
