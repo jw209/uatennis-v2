6e56fa67156374275186a2cb493be83b1c6b2cc1
jw209 <jw209@uakron.edu>
2021-11-29 23:27:40 -0500

    Fix heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const path = require('path');
 
 const accounts = require("./routes/api/accounts");
 const comments = require("./routes/api/comments");
@@ -31,6 +32,16 @@ app.use("/api/accounts", accounts);
 app.use("/api/comments", comments);
 app.use("/api/players", players);
 app.use("/api/games", games);
+
+// Server static assets 
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
